9d709945ed60a5247138d3c3257029f702ee70c3
Paul Gellai <paul.gellai@carvana.com>
2023-04-05 12:29:54 -0700

    add context. gpt4 support

--- testmycode.py
+++ testmycode.py
@@ -6,10 +6,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Test my code')
 parser.add_argument('-f', '--file', help='File to test', required=True)
 parser.add_argument('-o', '--output', help='Output file', required=True)
+parser.add_argument('-li', '--log-input', help='Log input', required=False, default=False)
+parser.add_argument('-c', '--context', help='Context', required=False, action="append", default=[])
+parser.add_argument('-m', '--model', help='Model', required=False, default="gpt-3.5-turbo")
 
 args = parser.parse_args()
 file_path = args.file
 output_file = args.output
+log_input = args.log_input
+context = args.context
 
 file = open(file_path, "r")
 file_data = file.read()
@@ -24,7 +29,10 @@ Please write some unit tests for this code. Requirements are:
 - Tests must also use Moq for mocking services
 - Tests make use of the [AutoMoqData] attribute which will automatically create test data
 - Tests should use FluentAssertions instead of Assert.Equal, for example result.Succeeded().Should().BeTrue();
-- Do not mock the Logger or automapper
+- Tests should use .Verifiable() at the end of ReturnsAsync() or ThrowsAsync()
+- Use .Verify() and .VerifyNoOtherCalls() to verify that the mock was called
+- Do not mock the Logger or Mapper services
+- When returning null in a mock setup, use `() => null` instead of `null`
 
 Here is an example of a stub for a successful unit test collection:
 ```csharp
@@ -65,20 +73,27 @@ public class HandlerTests
     }
 }
 ```
-
 """
 
-input_file = open("input.md", "w")
-input_file.write(data)
-input_file.close()
+if context:
+    data += "\n" + "Here is additional context.\n\n"
+    for c in context:
+        data += c + "\n\n"
+        context_file = open(c, "r")
+        data += "```csharp\n" + context_file.read() + "\n```\n"
+
+if log_input:
+    input_file = open("input.md", "w")
+    input_file.write(data)
+    input_file.close()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model=args.model,
     messages=[
         {"role": "user", "content": data}
     ],
-    temperature=0,
+    temperature=0.05,
 )
 
 print(response['choices'][0]['finish_reason'])
